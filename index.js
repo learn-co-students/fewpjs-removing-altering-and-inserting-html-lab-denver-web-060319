5be97eac222ccea761eca730ad84e34b1f3fc2af
JangmiTokki <13933554+JangmiTokki@users.noreply.github.com>
2019-07-16 08:29:55 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+main.remove();
+let newHeader = document.createElement('h1');
+newHeader.id = "victory";
+document.querySelector('h1#victory');
+newHeader.innerHTML = "is the champion"
